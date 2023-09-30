7c0c7b3dc4ff66f2c14af14dd49fefd414ad16fc
leogalgano99 <74017487+leogalgano99@users.noreply.github.com>
2023-09-30 11:34:27 +0200

    Delete ueransim directory

--- ueransim/network_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ip_addr=$(ip addr show uesimtun0 | grep -o 'inet [0-9.]*' | awk '{print $2}') && \
-ip route del default && \
-ip route add default via $ip_addr && \
-ip route add $ASTERISK_IP via $ip_addr dev uesimtun0
