969768efd93a57a5de7ebe0687d01c5b5e631481
leogalgano99 <leonardo.galgano@gmail.com>
2023-10-03 15:26:00 +0200

    Adattamento a Windows

---
+++ script/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# build of Open5GS and UERANSIM
+cd ../open5gs/base 
+docker build --no-cache --force-rm -t docker_open5gs . && \
+
+cd ../ueransim
+docker build --no-cache --force-rm -t docker_ueransim . && \
+
+cd ..
+# Mostra un menu con due opzioni
+PS3="Select an option: "
+options=("ARM" "Windows AMD64" "Exit")
+
+select choice in "${options[@]}"; do
+    case $choice in
+        1)
+            echo "You chose the installation for ARM processors"
+            set -a 
+            source .env
+            docker-compose build --no-cache && \
+            cd ../openli-training-lab
+            cd ./agency
+            docker build --platform linux/amd64 --no-cache --force-rm -t openli-agency . && \
+            cd ../collector
+            docker build --platform linux/amd64 --no-cache --force-rm -t openli-collector . && \
+            cd ../mediator
+            docker build --platform linux/amd64 --no-cache --force-rm -t openli-mediator . && \
+            cd ../provisioner
+            docker build --platform linux/amd64 --no-cache --force-rm -t openli-provisioner . && \
+            ;;
+        2)
+            echo "You chose the installation for AMD64 processors"
+            call env.bat
+            docker-compose build --no-cache && \
+            cd ../openli-training-lab
+            cd ./agency
+            docker build --no-cache --force-rm -t openli-agency . && \
+            cd ../collector
+            docker build --no-cache --force-rm -t openli-collector . && \
+            cd ../mediator
+            docker build --no-cache --force-rm -t openli-mediator . && \
+            cd ../provisioner
+            docker build --no-cache --force-rm -t openli-provisioner . 
+            ;;
+        3)
+            echo "Exiting..."
+            break
+            ;;
+        *)
+            echo "Invalid choice, try again"
+            ;;
+    esac
+done
+cd ../../open5gs/
+docker-compose build --no-cache && \
+cd ./asterisk
+docker build -t asterisk .
+
+echo "Build completed"
+
