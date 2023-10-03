0cfec72a58a37fe6a2b6338880009f67b943d65c
leogalgano99 <leonardo.galgano@gmail.com>
2023-10-03 12:07:13 +0200

    modifica Tshark

--- script/configure_openli.sh
+++ script/configure_openli.sh
@@ -4,6 +4,4 @@
 docker exec provisioner ./provisioner-start.sh && \
 docker exec mediator ./mediator-start.sh && \
 docker exec collector ./collector-start.sh && \
-docker exec provisioner ./rest.sh && \
-docker exec upf apt-get update && \
-docker exec upf apt-get install -y tshark
+docker exec provisioner ./rest.sh  
