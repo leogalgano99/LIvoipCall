b3f361328576db49eb055c305ae5cc8bdec62ffb
leogalgano99 <leonardo.galgano@gmail.com>
2023-10-04 14:29:46 +0200

    modifica 2

--- script/build.sh
+++ script/build.sh
@@ -3,14 +3,18 @@
 # build of Open5GS and UERANSIM
 cd ../open5gs/base 
 docker build --no-cache --force-rm -t docker_open5gs . && \
+echo "Open5GS build completed"
 
 cd ../ueransim
 docker build --no-cache --force-rm -t docker_ueransim . && \
+echo "UERANSIM build completed"
 
 cd ..
 set -a 
 source .env
 docker-compose build --no-cache && \
+echo "Open5GS and UERANSIM build completed"
+
 # Mostra un menu con due opzioni
 PS3="Select an option: "
 options=("ARM" "AMD64" "Exit")
@@ -28,6 +32,7 @@ select choice in "${options[@]}"; do
             docker build --platform linux/amd64 --no-cache --force-rm -t openli-mediator . && \
             cd ../provisioner
             docker build --platform linux/amd64 --no-cache --force-rm -t openli-provisioner . && \
+            echo "OpenLI build completed"
             ;;
         2)
             echo "You chose the installation for AMD64 processors"
@@ -39,7 +44,8 @@ select choice in "${options[@]}"; do
             cd ../mediator
             docker build --no-cache --force-rm -t openli-mediator . && \
             cd ../provisioner
-            docker build --no-cache --force-rm -t openli-provisioner . 
+            docker build --no-cache --force-rm -t openli-provisioner . && \
+            echo "OpenLI build completed"
             ;;
         3)
             echo "Exiting..."
