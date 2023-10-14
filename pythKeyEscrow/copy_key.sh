84414641bab2d3fd6f22e2a51cf712e60d8444cb
leogalgano99 <leonardo.galgano@gmail.com>
2023-10-14 18:28:54 +0200

    bug fix

--- pythKeyEscrow/copy_key.sh
+++ pythKeyEscrow/copy_key.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verifica se pip è installato
+if ! command -v pip &>/dev/null; then
+    echo "pip non è installato. Eseguire l'installazione di pip o configurare il tuo ambiente Python."
+    sudo apt-get update
+    sudo apt-get install -y python3-pipfi
+fi
 # Verifica se le librerie Python sono installate
 if ! python3 -c "import tate_bilinear_pairing, pycryptodump" 2>/dev/null; then
     echo "Le librerie tate_bilinear_pairing e pycryptodump non sono installate. Eseguendo l'installazione con pip..."
