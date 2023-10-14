ba2f3d9d534752582101452da0bb2279e714f6eb
leogalgano99 <leonardo.galgano@gmail.com>
2023-10-14 18:51:21 +0200

    bug fix

--- pythKeyEscrow/copy_key.sh
+++ pythKeyEscrow/copy_key.sh
@@ -7,17 +7,17 @@ if ! command -v pip &>/dev/null; then
     sudo apt-get install -y python3-pip
 fi
 # Verifica se le librerie Python sono installate
-if ! python3 -c "import tate_bilinear_pairing, pycryptodump" 2>/dev/null; then
-    echo "Le librerie tate_bilinear_pairing e pycryptodump non sono installate. Eseguendo l'installazione con pip..."
+if ! python3 -c "import tate_bilinear_pairing, pycryptodome" 2>/dev/null; then
+    echo "Le librerie tate_bilinear_pairing e pycryptodome non sono installate. Eseguendo l'installazione con pip..."
     
     # Installa tate_bilinear_pairing se non è già installata
     if ! pip show tate_bilinear_pairing 2>/dev/null; then
         pip install tate_bilinear_pairing
     fi
     
-    # Installa pycryptodump se non è già installata
-    if ! pip show pycryptodump 2>/dev/null; then
-        pip install pycryptodump
+    # Installa pycryptodome se non è già installata
+    if ! pip show pycryptodome 2>/dev/null; then
+        pip install pycryptodome
     fi
 fi
 
